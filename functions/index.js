39a902de2bb49931becbe49348db8c6bbf44e6d0
Thomas Smith <thsm@umich.edu>
2019-09-04 15:27:05 -0400

    Remove helloWorld and add startGame cloud function

--- functions/index.js
+++ functions/index.js
@@ -7,11 +7,21 @@ const firestore = admin.firestore();
 const gamesRef = firestore.collection("games");
 const usersRef = firestore.collection("users");
 
-exports.helloWorld = functions.https.onRequest(async (request, response) => {
-  const doc = await firestore.doc("games/77bv5AwBf5UpNpfNd2R4").get();
-  const name = doc.get("name");
-  response.send("Hello, world! Game has name \"" + name.toString() + "\".");
-});
+const MIN_PLAYERS = 3;
+
+// TODO: Parallelize all these reads/writes
+
+// Implementation of Fisher-Yates shuffle, based on
+//  https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+const shuffle = (array) => {
+  let temp, swapIndex;
+  for (let i = array.length - 1; i >= 0; --i) {
+    swapIndex = Math.floor(Math.random() * (i + 1));
+    temp = array[i];
+    array[i] = array[swapIndex];
+    array[swapIndex] = temp;
+  }
+};
 
 exports.createGame = functions.https.onCall(async (data, context) => {
   // TODO: Make idempotent
@@ -35,5 +45,66 @@ exports.createGame = functions.https.onCall(async (data, context) => {
 
   return {
     gameID: documentRef.id,
+  };
+});
+
+exports.startGame = functions.https.onCall(async (data, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError(
+      "auth-failed", "No authentication was provided"
+    );
+  }
+  const uid = context.auth.uid;
+  const gameID = data.gameID;
+  if (typeof gameID !== "string" || gameID.test(/[^0-9a-zA-Z]/)) {
+    throw new functions.https.HttpsError(
+      "invalid-argument", "Invalid (or no) gameID provided to startGame"
+    );
+  }
+  const gameRef = gamesRef.doc(gameID);
+  const currentGame = await gameRef.get();
+  if (!currentGame.exists) {
+    throw new functions.https.HttpsError(
+      "invalid-argument", "Invalid (or no) gameID provided to startGame"
+    );
+  }
+  if (currentGame.get("status") !== "notStarted") {
+    throw new functions.https.HttpsError(
+      "invalid-state", "Cannot start a game that has already been started"
+    );
+  }
+  const gamePlayersRef = gameRef.collection("players");
+  const userInGameRef = gamePlayersRef.doc(uid);
+  const userInGameData = await userInGameRef.get();
+  if (!userInGameData.exists || !userInGameData.get("isOwner")) {
+    throw new functions.https.HttpsError(
+      "auth-failed", "User does not have authentication to start game"
+    );
+  }
+  const playerRefs = await gamePlayersRef.listDocuments();
+  if (playerRefs.length < MIN_PLAYERS) {
+    throw new functions.https.HttpsError(
+      "invalid-state", "Cannot start a game with fewer than " + MIN_PLAYERS +
+        " players."
+    );
+  }
+  shuffle(playerRefs);
+  await Promise.all(playerRefs.map((playerRef, i) => {
+    return playerRef.update({
+      alive: true,
+      kills: 0,
+      pendingVotes: [],
+      sniper: i === 0 ? playerRefs[playerRefs.length - 1] : playerRefs[i - 1],
+      target: i === playerRefs.length - 1 ? playerRefs[0] : playerRefs[i + 1]
+    });
+  }));
+
+  await gameRef.update({
+    startTime: new Date(),
+    status: "started"
+  });
+
+  return {
+    gameID: gameID
   };
 });
