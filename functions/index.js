26feeadb5569e4a6fb9b9b5390ee8ef5fe4645b8
Vincent Nagel <vnagel@umich.edu>
2019-10-06 16:02:57 -0400

    Removed databaseURL field from initializeApp
    I don't believe this is necessary. The documentation says this is the
    url for the Realtime Database, but we use Cloud Firestore. I put this in
    earlier because it was in the documentation for setting up the service
    account credentials

--- functions/index.js
+++ functions/index.js
@@ -2,8 +2,7 @@ const admin = require("firebase-admin");
 const serviceAccount = require("./photo-assassin-adminsdk-service-account-key.json")
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: "photo-assassin.appspot.com",
-  databaseURL: "https://photo-assassin.firebaseio.com"
+  storageBucket: "photo-assassin.appspot.com"
 });
 
 // TODO: Parallelize all reads/writes
